f9241e3f32f518c2e0fe0e37f47fad0544f3c593
Martin Kobík <75179174+MKprojekt@users.noreply.github.com>
2020-12-05 23:00:46 +0100

    vylepšení robota
    Robot má distance sensor a lidar.

--- controllers/Mr_controller/Mr_controller.m
+++ controllers/Mr_controller/Mr_controller.m
@@ -15,19 +15,39 @@ TIME_STEP = 64;
 % get and enable devices, e.g.:
 %  camera = wb_robot_get_device('camera');
 %  wb_camera_enable(camera, TIME_STEP);
+
+%GET DEVICE
 motor_left = wb_robot_get_device('motor_left');
 motor_right = wb_robot_get_device('motor_right');
 
+DS = wb_robot_get_device('distance_sensor');
+
+lidar = wb_robot_get_device('lidar');
+
+%SET
 wb_motor_set_position(motor_left, inf);
 wb_motor_set_velocity(motor_left, 1);
 wb_motor_set_position(motor_right, inf);
 wb_motor_set_velocity(motor_right, 1);
+
+%ENABLE
+wb_distance_sensor_enable(DS, TIME_STEP);
+
+wb_lidar_enable(lidar, TIME_STEP);
+wb_lidar_enable_point_cloud(lidar);
 
 % main loop:
 % perform simulation steps of TIME_STEP milliseconds
 % and leave the loop when Webots signals the termination
 %
 while wb_robot_step(TIME_STEP) ~= -1
+
+distance = wb_distance_sensor_get_value(DS);
+
+if distance < 100
+wb_motor_set_velocity(motor_left, 0);
+wb_motor_set_velocity(motor_right, 0);
+end
 
   % read the sensors, e.g.:
   %  rgb = wb_camera_get_image(camera);
