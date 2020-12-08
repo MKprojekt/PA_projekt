5518d6fc6df1898de6024c3d7b3ab5036ddf5f4b
Martin Kobík <75179174+MKprojekt@users.noreply.github.com>
2020-12-08 21:58:48 +0100

    Robot Lidar
    Robot aktivně získává data z lidaru. (data zatím nejsou využita)

--- controllers/Mr_controller/Mr_controller.m
+++ controllers/Mr_controller/Mr_controller.m
@@ -28,13 +28,15 @@ lidar = wb_robot_get_device('lidar');
 wb_motor_set_position(motor_left, inf);
 wb_motor_set_velocity(motor_left, 1);
 wb_motor_set_position(motor_right, inf);
-wb_motor_set_velocity(motor_right, 1);
+wb_motor_set_velocity(motor_right, 2);
 
 %ENABLE
 wb_distance_sensor_enable(DS, TIME_STEP);
 
 wb_lidar_enable(lidar, TIME_STEP);
 wb_lidar_enable_point_cloud(lidar);
+
+%GET
 
 % main loop:
 % perform simulation steps of TIME_STEP milliseconds
@@ -43,6 +45,7 @@ wb_lidar_enable_point_cloud(lidar);
 while wb_robot_step(TIME_STEP) ~= -1
 
 distance = wb_distance_sensor_get_value(DS);
+points = wb_lidar_get_point_cloud(lidar)
 
 if distance < 100
 wb_motor_set_velocity(motor_left, 0);
