182cd6c4fff1def6edbaf1f1e8e927570a2b68fe
Martin Kobík <75179174+MKprojekt@users.noreply.github.com>
2020-12-10 00:20:44 +0100

    Test

--- controllers/Mr_controller/Mr_controller.m
+++ controllers/Mr_controller/Mr_controller.m
@@ -22,19 +22,14 @@ motor_right = wb_robot_get_device('motor_right');
 
 DS = wb_robot_get_device('distance_sensor');
 
-lidar = wb_robot_get_device('lidar');
-
 %SET
 wb_motor_set_position(motor_left, inf);
-wb_motor_set_velocity(motor_left, 1);
+wb_motor_set_velocity(motor_left, 3);
 wb_motor_set_position(motor_right, inf);
-wb_motor_set_velocity(motor_right, 2);
+wb_motor_set_velocity(motor_right, 3);
 
 %ENABLE
 wb_distance_sensor_enable(DS, TIME_STEP);
-
-wb_lidar_enable(lidar, TIME_STEP);
-wb_lidar_enable_point_cloud(lidar);
 
 %GET
 
@@ -42,18 +37,20 @@ wb_lidar_enable_point_cloud(lidar);
 % perform simulation steps of TIME_STEP milliseconds
 % and leave the loop when Webots signals the termination
 %
+
 while wb_robot_step(TIME_STEP) ~= -1
 
 distance = wb_distance_sensor_get_value(DS);
-points = wb_lidar_get_point_cloud(lidar)
 
 if distance < 100
-wb_motor_set_velocity(motor_left, 0);
-wb_motor_set_velocity(motor_right, 0);
+wb_motor_set_position(motor_right, -3);
+wb_motor_set_position(motor_left, 3);
 end
+
 
   % read the sensors, e.g.:
   %  rgb = wb_camera_get_image(camera);
+  
 
   % Process here sensor data, images, etc.
 
